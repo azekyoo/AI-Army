61b05d7437d2da9ce7dd5a08cf5c52cddbb7374e
Thomas Ploix <thomas.ploix@epfedu.fr>
2024-12-18 01:18:47 +0100

    Ajout bouton Plus de coins

--- Assets/Scripts/Army/Castle.cs
+++ Assets/Scripts/Army/Castle.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,9 @@ public class Castle : MonoBehaviour
     public Transform spawnPoint;
 
     [Header("Resources")] 
-    private int currentCoins = 200; // Montant initial de pièces
+    private int currentCoins = 150; // Montant initial de pièces
 
-
-    public Text coinsText;
+    [SerializeField] TMP_Text m_CoinsText;
 
     void Start()
     {
@@ -32,16 +32,17 @@ public class Castle : MonoBehaviour
             currentCoins -= unitCost;
             UpdateCoinsDisplay();
             SpawnUnit();
+            
         }
         else
         {
-            Debug.Log("Pas assez de pièces ou production en cours!");
+            //Debug.Log("Pas assez de pièces ou production en cours!");
         }
     }
 
     private void SpawnUnit()
     {
-        if (spawnPoint != null && unitPrefab != null)
+        if (spawnPoint && unitPrefab)
         {
             // Spawn l'unité au point de spawn
             Instantiate(unitPrefab, spawnPoint.position, spawnPoint.rotation);
@@ -53,12 +54,18 @@ public class Castle : MonoBehaviour
         currentCoins += amount;
         UpdateCoinsDisplay();
     }
+    public void AddCoins50()
+    {
+        currentCoins = currentCoins + 50;
+        Debug.Log("TEST BOUTON! : " + currentCoins);
+        UpdateCoinsDisplay();
+    }
 
     private void UpdateCoinsDisplay()
     {
-        if (coinsText != null)
+        if (m_CoinsText)
         {
-            coinsText.text = $"Pièces: {currentCoins}";
+            m_CoinsText.text = $"{currentCoins}";
         }
     }
 }
